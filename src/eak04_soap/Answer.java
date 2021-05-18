74b7b409adaad0869e695866f931d7e1c6fbe3b0
AlmasEgeubai <75420211+AlmasEgeubai@users.noreply.github.com>
2021-05-18 08:21:41 +0600

    Add files via upload

---
+++ src/eak04_soap/Answer.java
@@ -0,0 +1,25 @@
+package eak04_soap;
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+
+    private double y;
+
+    public Answer() {
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\nОтвет: Y = %.3f", y);
+    }
+
+}
---
+++ src/eak04_soap/ICommunicationWithClient.java
@@ -0,0 +1,11 @@
+package eak04_soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface ICommunicationWithClient {
+
+    @WebMethod
+    public Answer solution(double a, double b, double x);
+}
